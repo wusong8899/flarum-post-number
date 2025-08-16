0638b704eeeaa3fc28b863f238f0e0b5792dfd7e
wusong8899 <wusong8899@users.noreply.github.com>
2025-08-16 16:05:28 +0900

    f

--- extend.php
+++ extend.php
@@ -3,6 +3,6 @@
 use Flarum\Extend;
 
 return [
-    (new Extend\Frontend('forum'))->js(__DIR__ . '/js/dist/forum.js')->css(__DIR__.'/less/forum.less'),
+    (new Extend\Frontend('forum'))->js(__DIR__ . '/js/dist/forum.js')->css(__DIR__ . '/less/forum.less'),
     (new Extend\Locales(__DIR__ . '/locale'))
 ];
